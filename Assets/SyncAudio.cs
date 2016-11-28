0e39ff6a6ae6cf48359ab637859549b1cb27ec66
charder <chardergaming@gmail.com>
2016-11-28 18:12:28 -0500

    Syncing of audio is in place.
    Test Audio and everything. WOW.

---
+++ Assets/SyncAudio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SyncAudio : MonoBehaviour {
+	public float beats; //bpm
+	public int indColor; //color array index
+	public Color[] colorA; //array of colors
+	public float timeChange;
+	public UnityStandardAssets.ImageEffects.EdgeDetectionColor colorChange;
+	// Use this for initialization
+	void Start () {
+		colorChange.edgesColor = colorA [indColor];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeChange += Time.deltaTime;
+		if (timeChange >= 60 / beats) {
+			timeChange -= (60/beats);
+			indColor++;
+			if (indColor >= colorA.Length) {
+				indColor = 0;
+			}
+			colorChange.edgesColor = colorA [indColor];
+		}
+	
+	}
+}
