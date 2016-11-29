6ecc6c3814f3553ed11b4546a61feab460a068eb
charder <chardergaming@gmail.com>
2016-11-29 11:44:02 -0500

    Progress on Locks and Doors and stuff.

---
+++ Assets/LockedDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockedDoor : MonoBehaviour {
+	public GameObject[] locks; //references to the visual locks
+	bool[] lockBool; //whether unlocked or locked
+	// Use this for initialization
+	void Start () {
+		lockBool = new bool[locks.Length];
+		for (int i = 0; i < lockBool.Length; i++) { //set all locks to false
+			lockBool[i] = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
